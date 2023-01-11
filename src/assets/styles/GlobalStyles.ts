4eda2709a40b4b7da55b7ad110bf603a698be958
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-01-10 23:44:15 -0300

    file restructuring

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
-import colors from './colorVariable';
- 
+
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     font-family: Open-Sans, Helvetica, Sans-Serif;
-    background-color: ${colors.body};
   }
 `;
  
